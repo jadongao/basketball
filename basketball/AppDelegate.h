6bcce250c43762f8f35d3c2fae43474075c1fd03
xiaobocs <42482974+xiaobocs@users.noreply.github.com>
2023-10-03 12:42:44 +0800

    Initial commit

---
+++ basketball/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  basketball
+//
+//  Created by ellen on 2023/10/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
